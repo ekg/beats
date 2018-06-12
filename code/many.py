3473278627b4c8208d52cfce9107ab7cce16a72f
Erik Garrison <erik.garrison@gmail.com>
2018-06-12 18:32:59 +0200

    updates

--- code/many.py
+++ code/many.py
@@ -4,3 +4,12 @@ Clock.update_tempo(124)
 d2 >> play(" .n (h ) ", dur=PDur(3,8))
 d1 >> play("[x-]", dur=PEuclid(5,8))
 p1 >> marimba([P/(0,2,4,7)]-P[12], dur=2)
+
+print(SynthDefs)
+
+Clock.update_tempo(119)
+d0 >> play("  s ", sample=8, dur=1)
+d1 >> play("x-(o o[-a])",sample=var([1,2,3],0.23))
+
+d2.stop()
+d2 >> play(" o(* *[ai])",sample=0,dur=PDur(2,9))
