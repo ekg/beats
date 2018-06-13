38b070b9b9ef6352ce005a23251a9431f9b88e96
Erik Garrison <erik.garrison@gmail.com>
2018-06-13 12:48:45 +0200

    do rounds

--- code/xdda.py
+++ code/xdda.py
@@ -4,10 +4,15 @@ d1 >> play(P["V  V"] & P[" *  "] & P["[ m] "])
 p1 >> keys([(0,2,5)]+[1,3], dur=[rest(2),2]) # needs work
 p2 >> blip(PTri(8), dur=PDur(3,8).stretch(8)|P[rest(1)])
 
+b1.stop()
+d1.stop()
+p1.stop()
+p2.stop()
+
 # modulations
 p1 >> piano(P[(0,2,5)]|[1,3], dur=[rest(2),2], amp=0.8, echo=3)
 p1 >> piano([(0,2,5)]+[1,3], dur=P[rest(2)]|PDur(3,8), amp=0.8, echo=3)
-p2 >> blip(PTri(8), dur=PDur(3,8).stretch(7)|P[rest(1)], echo=1.75)
+p2 >> blip(PTri(8), dur=PDur(3,8).stretch(8)|P[rest(1)], echo=1.75)
 
 print(PDur(3,8)|P[rest(1)])
 print(PDur(3,8).stretch(7)|P[rest(1)])
